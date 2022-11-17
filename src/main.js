c6b926d933b5c183a0ec120675a717fd7f36a735
Polyakova Maria <polyakovamy@yandex.ru>
2022-11-17 21:38:19 +0300

    Верстка секции head

--- src/main.js
+++ src/main.js
@@ -1,2 +1,26 @@
 import "./main.scss";
-console.log("Hello main.js!");
+
+import $ from "jquery";
+import "slick-slider";
+
+$(function () {
+  $(".top-slider").slick({
+    dots: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    prevArrow: '<button type="button" class="slick-arrow slick-prev"></button>',
+    nextArrow: '<button type="button" class="slick-arrow slick-next"></button>',
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {},
+      },
+      {
+        breakpoint: 640,
+        settings: {
+          arrows: false,
+        },
+      },
+    ],
+  });
+});
